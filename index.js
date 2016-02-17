b72de2e1543cda9c0046d6fa1db6c9ae63dd0622
Fatih Kalifa <fatihkalifa@gmail.com>
2016-02-17 20:40:47 +0700

    Refactor handleOpts into own file (need test)

--- index.js
+++ index.js
@@ -4,66 +4,12 @@
 var path = require('path');
 var eslint = require('eslint');
 var globby = require('globby');
-var objectAssign = require('object-assign');
-var arrify = require('arrify');
-var pkgConf = require('pkg-conf');
-var deepAssign = require('deep-assign');
-
-var DEFAULT_IGNORE = [
-  'node_modules/**',
-  'bower_components/**',
-  'coverage/**',
-  '{tmp,temp}/**',
-  '**/*.min.js',
-  '**/bundle.js',
-  'fixture.js',
-  '{test/,}fixture{s,}/**'
-];
-
-var DEFAULT_CONFIG = {
-  useEslintrc: false,
-  baseConfig: {
-    extends: 'marlint'
-  }
-};
-
-function handleOpts(opts) {
-  opts = objectAssign({
-    cwd: process.cwd()
-  }, opts);
-
-  opts = objectAssign({}, pkgConf.sync('marlint', opts.cwd), opts);
-
-  // alias to help humans
-  opts.envs = opts.envs || opts.env;
-  opts.globals = opts.globals || opts.global;
-  opts.ignores = opts.ignores || opts.ignore;
-  opts.rules = opts.rules || opts.rule;
-
-  opts.ignores = DEFAULT_IGNORE.concat(opts.ignores || []);
-
-  opts._config = deepAssign({}, DEFAULT_CONFIG, {
-    envs: arrify(opts.envs),
-    globals: arrify(opts.globals),
-    rules: opts.rules
-  });
-
-  if (!opts._config.rules) {
-    opts._config.rules = {};
-  }
-
-  if (opts.es5) {
-    opts._config.baseConfig = 'marlint/es5';
-  }
-
-  return opts;
-}
+var handleOpts = require('./handle-opts');
 
 exports.lintText = function (str, opts) {
   opts = handleOpts(opts);
 
   var engine = new eslint.CLIEngine(opts._config);
-
   return engine.executeOnText(str, opts.filename);
 };
 
@@ -82,7 +28,6 @@ exports.lintFiles = function (patterns, opts) {
     });
 
     var engine = new eslint.CLIEngine(opts._config);
-
     return engine.executeOnFiles(paths);
   });
 };
